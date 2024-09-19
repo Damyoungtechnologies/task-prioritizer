5828d16a63906481a2ddd6f98ace2292b95c7400
damyoungtechnologies <damyounggraphics@gmail.com>
2024-09-19 19:13:14 +0100

    App has been update with the Get Started, Login, Register and Homepage. this will help to navigate between pagees using Broswer Router and useLocation imported from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, useLocation } from "react-router-dom"; // Import BrowserRouter and useLocation
+import { AuthenticationProvider } from "./contextManager/authContextManager";
+import { useRoutes } from "react-router-dom";
 import './App.css';
+import { GetStarted, SignInPage, Register, Homepage, NavigationBar } from "./components";
+
+// Create a separate component for Routes
+function AppRoutes() {
+  const appRoutes = [
+    {
+      path: "*", // This will match all undefined paths
+      element: <GetStarted />,
+    },
+    {
+      path: "/Login",
+      element: <SignInPage />,
+    },
+    {
+      path: "/Register",
+      element: <Register />,
+    },
+    {
+      path: "/Homepage",
+      element: <Homepage />,
+    },
+  ];
+
+  return useRoutes(appRoutes);
+}
+
+function Layout() {
+  const location = useLocation();
+
+  // Conditionally show the NavigationBar only if the route is NOT GetStarted
+  const hideNavbarOnPaths = ["/", "/GetStarted"];
+  const shouldShowNavbar = !hideNavbarOnPaths.includes(location.pathname);
+
+  return (
+    <div className="App">
+      {/* Conditionally render the NavigationBar based on the current route */}
+      {shouldShowNavbar && <NavigationBar />}
+
+      <div className="app-container">
+        <AppRoutes />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>clear
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthenticationProvider>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </AuthenticationProvider>
   );
 }
 
